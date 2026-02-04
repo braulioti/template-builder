38cf3f597a7afe0b2be9db51a776ed603a91da11
Bráulio Figueiredo <braulio@braulioti.com.br>
2026-02-04 12:17:32 -0300

    refactor: #26 Resolved cyclomatic complexity in CLINavigate::runChecklistLoop

--- src/cli-utils/CLINavigate.cpp
+++ src/cli-utils/CLINavigate.cpp
@@ -56,36 +56,64 @@ bool readConsoleKey(unsigned short& key, bool& keyPressed) {
 } // namespace
 #endif
 
-void CLINavigate::runChecklistLoop(ChecklistLoopParams& params) {
+namespace {
+
+void renderChecklist(const ChecklistLoopParams& params) {
     const auto& options = params.promptInput->getOptions();
-    while (!params.done) {
-        for (size_t i = 0; i < options.size(); ++i) {
-            std::cout << (i == params.currentIndex ? "> " : "  ");
-            std::cout << (params.selected[i] ? "[ X ] " : "[   ] ");
-            std::cout << options[i]->getName() << std::endl;
-        }
-        std::cout << "Use Up/Down arrows to navigate, Space to select/deselect, Enter to confirm" << std::endl;
+    for (size_t i = 0; i < options.size(); ++i) {
+        std::cout << (i == params.currentIndex ? "> " : "  ");
+        std::cout << (params.selected[i] ? "[ X ] " : "[   ] ");
+        std::cout << options[i]->getName() << std::endl;
+    }
+    std::cout << "Use Up/Down arrows to navigate, Space to select/deselect, Enter to confirm" << std::endl;
+}
+
+void clearDisplayedLines(size_t lineCount) {
+    for (size_t i = 0; i < lineCount; ++i) {
+        std::cout << "\033[A\033[2K";
+    }
+}
+
 #ifdef _WIN32
-        while (true) {
-            unsigned short key = 0;
-            bool keyPressed = false;
-            if (readConsoleKey(key, keyPressed)) {
-                for (size_t i = 0; i < options.size() + 1; ++i) {
-                    std::cout << "\033[A\033[2K";
-                }
-                if (key == VK_UP && params.currentIndex > 0) {
-                    --params.currentIndex;
-                } else if (key == VK_DOWN && params.currentIndex < options.size() - 1) {
-                    ++params.currentIndex;
-                } else if (key == VK_SPACE) {
-                    params.selected[params.currentIndex] = !params.selected[params.currentIndex];
-                } else if (key == VK_RETURN) {
-                    params.done = true;
-                }
-                break;
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        }
+void handleKeyPress(ChecklistLoopParams& params, unsigned short key) {
+    const auto& options = params.promptInput->getOptions();
+    if (key == VK_UP && params.currentIndex > 0) {
+        --params.currentIndex;
+    } else if (key == VK_DOWN && params.currentIndex < options.size() - 1) {
+        ++params.currentIndex;
+    } else if (key == VK_SPACE) {
+        params.selected[params.currentIndex] = !params.selected[params.currentIndex];
+    } else if (key == VK_RETURN) {
+        params.done = true;
+    }
+}
+
+bool processSingleKeyInput(ChecklistLoopParams& params) {
+    unsigned short key = 0;
+    bool keyPressed = false;
+    if (!readConsoleKey(key, keyPressed)) {
+        return false;
+    }
+    const auto& options = params.promptInput->getOptions();
+    clearDisplayedLines(options.size() + 1);
+    handleKeyPress(params, key);
+    return true;
+}
+
+void waitForAndProcessKey(ChecklistLoopParams& params) {
+    while (!processSingleKeyInput(params)) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+#endif
+
+} // namespace
+
+void CLINavigate::runChecklistLoop(ChecklistLoopParams& params) {
+    while (!params.done) {
+        renderChecklist(params);
+#ifdef _WIN32
+        waitForAndProcessKey(params);
 #else
         params.done = true;
 #endif
